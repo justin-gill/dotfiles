58c177cb89ff88887180a63e376d11a3ef769011
Justin Gill <47087703+justin-gill@users.noreply.github.com>
2023-05-28 14:17:24 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,33 +1,19 @@
 #!/bin/sh
-dnf update
+dnf update -y
 # add flatpak
-dnf install neovim alacritty git tmux npm gcc gcc-c++ flatpak
+dnf install neovim alacritty git tmux npm gcc gcc-c++ flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 # add rpm fusion
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
 # Delete these if you don't want them
 # Obsidian
-flatpak install flathub md.obsidian.Obsidian
+flatpak install -y --noninteractive flathub md.obsidian.Obsidian
 # Tidal
-flatpak install flathub com.mastermindzh.tidal-hifi
+flatpak install -y --noninteractive flathub com.mastermindzh.tidal-hifi
 # Brave
-dnf install dnf-plugins-core
+dnf install dnf-plugins-core -y
 dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-dnf install brave-browser
-
-# grim dependencies
-# some of these may be unneccessary
-dnf install meson ninja-build pixman libpng libjpeg cmake cairo-devel wayland-protocols-devel
-# grim
-git clone --depth 1 https://github.com/emersion/grim.git
-cd grim
-meson build
-ninja -C build
-install -m 755 build/grim /usr/local/bin
-cd ..
-### THE DANGER ZONE ###
-rm -rf grim/
-
+dnf install brave-browser -y
