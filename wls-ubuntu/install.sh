4082d44333291ded975ec1e40f2b240a649310b8
Justin Gill <47087703+justin-gill@users.noreply.github.com>
2023-05-28 18:22:49 -0700

    Update install.sh

--- wls-ubuntu/install.sh
+++ wls-ubuntu/install.sh
@@ -1,5 +1,3 @@
 # nvim
 add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt install neovim tmux gcc g++ -y
-# nvm, since default node version on ubuntu conflicts with Mason
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
