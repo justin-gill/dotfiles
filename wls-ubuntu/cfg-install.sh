dab7cce570b4583dfded670cc36806ed733da6a3
Justin Gill <47087703+justin-gill@users.noreply.github.com>
2023-05-28 18:35:48 -0700

    Update cfg-install.sh

--- wls-ubuntu/cfg-install.sh
+++ wls-ubuntu/cfg-install.sh
@@ -12,5 +12,5 @@ if [ $? = 0 ]; then
     echo "Backing up pre-existing dot files.";
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
-config checkout
+config checkout wsl-ubuntu
 config config status.showUntrackedFiles no
