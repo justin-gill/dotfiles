7b4a7c99bc8bd004c6c642850215fe316be12de2
Justin Gill <47087703+justin-gill@users.noreply.github.com>
2023-08-14 22:11:29 -0700

    Remove Brave

--- fedora-sway/install.sh
+++ fedora-sway/install.sh
@@ -12,8 +12,3 @@ dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-rel
 flatpak install -y --noninteractive flathub md.obsidian.Obsidian
 # Tidal
 flatpak install -y --noninteractive flathub com.mastermindzh.tidal-hifi
-# Brave
-dnf install dnf-plugins-core -y
-dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-dnf install brave-browser -y
