f45de45ebc11cc77cf96de6835e893dfa95cf35f
Justin Gill <47087703+justin-gill@users.noreply.github.com>
2023-08-15 17:27:25 -0700

    Removed backup dotfile creation

--- fedora-sway/cfg-install.sh
+++ fedora-sway/cfg-install.sh
@@ -4,13 +4,5 @@ git clone --bare git@github.com:justin-gill/dotfiles.git $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
-mkdir -p .config-backup
-config checkout sway-fedora
-if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
 config checkout sway-fedora
 config config status.showUntrackedFiles no
